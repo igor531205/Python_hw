300710c27476c58bf4ab4cd77c7ce27720950603
igor531205 <41549374+igor531205@users.noreply.github.com>
2023-08-17 23:05:00 +0300

    HW2 Task job

--- HW2/Task1.py
+++ HW2/Task1.py
@@ -1,3 +1,31 @@
-# Напишите программу, которая получает целое число и возвращает его
-# шестнадцатеричное строковое представление. Функцию hex используйте для
-# проверки своего результата.
+# Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
+# Функцию hex используйте для проверки своего результата.
+
+
+def user_input_number(message: str, lower_limit: int, upper_limit: int) -> int:
+    """Пользовательский ввод.
+    :param message: Сообщение пользователю.
+    :param lower_limit: Начальное значение ввода.
+    :param upper_limit: Конечное значение ввода.
+    :return: Введенное пользователем число.
+    """
+
+    while True:
+
+        _string = input(f'{message}')
+
+        if _string.isdigit():
+
+            _number = int(_string)
+
+            if lower_limit <= _number <= upper_limit:
+
+                return _number
+
+
+LOWER_LIMIT = -100_000
+UPPER_LIMIT = 100_000
+
+message_for_user = f'Please enter an integer between {LOWER_LIMIT} and {UPPER_LIMIT} -> '
+
+number_from_user = user_input_number(message_for_user, LOWER_LIMIT, UPPER_LIMIT)
--- HW2/Task2.py
+++ HW2/Task2.py
@@ -1,3 +1,26 @@
-# Напишите программу, которая принимает две строки вида “a/b” — дробь с
-# числителем и знаменателем. Программа должна возвращать сумму и *произведение
-# дробей. Для проверки своего кода используйте модуль fractions.
+# Напишите программу, которая принимает две строки вида “a/b” — дробь с числителем и знаменателем. Программа должна
+# возвращать сумму и *произведение дробей. Для проверки своего кода используйте модуль fractions.
+
+
+def user_input_fraction(message: str) -> list[int]:
+    """Пользовательский ввод дроби.
+    :param message: Сообщение пользователю.
+    :return: Введенная пользователем дробь.
+    """
+
+    while True:
+
+        _string = input(f'{message}')
+
+        if _string.isdigit():
+
+            _number = int(_string)
+
+            if lower_limit <= _number <= upper_limit:
+
+                return _number
+
+
+message_for_user = f'Please enter a fraction in the format a/b -> '
+
+fraction_from_user = user_input_fraction(message_for_user)
