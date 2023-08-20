bd690b388761f68da4984b85483b68acb05e2878
igor531205 <41549374+igor531205@users.noreply.github.com>
2023-08-20 19:19:21 +0300

    Task3 job

--- HW2/Task3.py
+++ HW2/Task3.py
@@ -14,22 +14,92 @@ from decimal import Decimal
 class Atm():
     """Банкомат (automated teller machine)"""
 
-    __MIN_WITHDRAWAL_COMMISSION__: Decimal = Decimal('30.00')
-    __WITHDRAWAL_COMMISSION_PERCENT__: Decimal = Decimal('1.50')
-    __MAX_WITHDRAWAL_COMMISSION__: Decimal = Decimal('600.00')
-    __MULTIPLICITY__: Decimal = Decimal('50.00')
+    _ZERRO_RUR: Decimal = Decimal('0.00')
+    _MULTIPLICITY_RUR: Decimal = Decimal('50.00')
+    _MIN_WITHDRAWAL_COMMISSION_RUR: Decimal = Decimal('30.00')
+    _MAX_WITHDRAWAL_COMMISSION_RUR: Decimal = Decimal('600.00')
+    _WEALTH_RUR: Decimal = Decimal('5_000_000.00')
+
+    _REMUNERATION_COUNT: Decimal = Decimal('3')
+
+    _FULL_PERCENT: Decimal = Decimal('100')
+    _WITHDRAWAL_COMMISSION_PERCENT: Decimal = Decimal('1.5')
+    _REMUNERATION_PERCENT: Decimal = Decimal('3')
+    _WEALTH_PERCENT: Decimal = Decimal('10')
 
     def __init__(self):
-        self.cash: Decimal = Decimal()
+        self._cash: Decimal = Decimal(self._ZERRO_RUR)
+
+    def _balance(self):
+        """Вывод суммы остатка средств (баланс).        
+        """
+        print(f'Your money: {self._cash} RUR\n')
+
+    def _user_input_decimal(self, message: str) -> Decimal:
+        """Пользовательский ввод суммы операции.
+        :param message: Сообщение пользователю.
+        :return: Сумма операции.
+        """
+
+        while True:
+
+            _string = input(f'{message}').replace(',', '.')
+
+            _decimal_args = _string.split('.')
+
+            _COUNT_OF_ARGUMENTS_DECIMAL = 2
+
+            if len(_decimal_args) <= _COUNT_OF_ARGUMENTS_DECIMAL and all(a.isdigit() for a in _decimal_args):
+
+                _decimal = Decimal(_string)
+                if _decimal > 0 and not _decimal % self._MULTIPLICITY_RUR:
+                    return _decimal
+                else:
+                    print(f'Cancel operation.')
+
+            else:
+                print('You entered the amount in the wrong format!')
 
     def replenish(self):
-        message_for_user = f'Enter the amount of ATM replenishment multiple {self.__MULTIPLICITY__} RUR -> '
+        """Операция пополнения.        
+        """
+        message_for_user = (f''' Enter the amount of ATM replenishment multiple {self._MULTIPLICITY_RUR} RUR -> ''')
+
+        self._cash += self._user_input_decimal(message_for_user)
+
+        self._balance()
 
     def withdraw(self):
-        message_for_user = f'Enter the amount of withdrawal from the ATM multiple {self.__MULTIPLICITY__} RUR -> '
+        """Операция снятия.        
+        """
+        message_for_user = (f''' Withdrawal commision of ATM replenishment - {self._WITHDRAWAL_COMMISSION_PERCENT}%.
+                           \r Minimum commission - {self._MIN_WITHDRAWAL_COMMISSION_RUR} RUR. 
+                           \r Maximum commission - {self._MAX_WITHDRAWAL_COMMISSION_RUR} RUR.
+                           \r Enter the amount of withdrawal from the ATM multiple {self._MULTIPLICITY_RUR} RUR -> ''')
+
+        _request = self._user_input_decimal(message_for_user)
+        _operation_commission = _request * self._WITHDRAWAL_COMMISSION_PERCENT / self._FULL_PERCENT
+
+        if _operation_commission < self._MIN_WITHDRAWAL_COMMISSION_RUR:
+            _operation_commission = self._MIN_WITHDRAWAL_COMMISSION_RUR
+
+        elif _operation_commission > self._MAX_WITHDRAWAL_COMMISSION_RUR:
+            _operation_commission = self._MAX_WITHDRAWAL_COMMISSION_RUR
+
+        _request_sum = _request + _operation_commission
+
+        if _request_sum <= self._cash:
+            self._cash -= _request_sum
+
+        else:
+            print(f'Cancel operation - Insufficient funds on the account')
+
+        self._balance()
 
     def close(self):
-        print(f'Your money: {self.cash} RUR')
+        """Выйти.        
+        """
+        self._balance()
         exit()
 
 
@@ -37,6 +107,29 @@ def show_menu() -> int:
     """Меню выбора действия.
     :return: Значение выбранного действия.
     """
+
+    def user_input_number(message: str, lower_limit: int, upper_limit: int) -> int:
+        """Пользовательский ввод.
+        :param message: Сообщение пользователю.
+        :param lower_limit: Начальное значение ввода.
+        :param upper_limit: Конечное значение ввода.
+        :return: Введенное пользователем число.
+        """
+
+        while True:
+
+            _string = input(f'{message}')
+
+            if _string.isdigit():
+
+                _number = int(_string)
+
+                if lower_limit <= _number <= upper_limit:
+
+                    print()
+                    return _number
+            print('You entered an invalid value\n')
+
     MIN_CHOICE = 0
     MAX_CHOICE = 2
     MESSAGE_FOR_USER = (f''' {'"'*10} Main menu: {'"'*10}
@@ -45,29 +138,6 @@ def show_menu() -> int:
                         \r 0. "Close the ATM"
                         \r Enter menu item number -> ''')
     return user_input_number(MESSAGE_FOR_USER, MIN_CHOICE, MAX_CHOICE)
-
-
-def user_input_number(message: str, lower_limit: int = 0, upper_limit: int = 5_000_000) -> int:
-    """Пользовательский ввод.
-    :param message: Сообщение пользователю.
-    :param lower_limit: Начальное значение ввода.
-    :param upper_limit: Конечное значение ввода.
-    :return: Введенное пользователем число.
-    """
-
-    while True:
-
-        _string = input(f'{message}')
-
-        if _string.isdigit():
-
-            _number = int(_string)
-
-            if lower_limit <= _number <= upper_limit:
-
-                print()
-                return _number
-        print('You entered an invalid value\n')
 
 
 REPLENISH: int = 1
