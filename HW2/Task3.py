cd0429ab54c5d72468765be74ecc6af804821a8b
igor531205 <41549374+igor531205@users.noreply.github.com>
2023-08-20 20:51:12 +0300

    Task3 complited

--- HW2/Task3.py
+++ HW2/Task3.py
@@ -20,7 +20,10 @@ class Atm():
     _MAX_WITHDRAWAL_COMMISSION_RUR: Decimal = Decimal('600.00')
     _WEALTH_RUR: Decimal = Decimal('5_000_000.00')
 
-    _REMUNERATION_COUNT: Decimal = Decimal('3')
+    _START_COUNT: int = int(0)
+    _REMUNERATION_COUNT: int = int(3)
+
+    _HUNDREDTH_PART: int = int(2)
 
     _FULL_PERCENT: Decimal = Decimal('100')
     _WITHDRAWAL_COMMISSION_PERCENT: Decimal = Decimal('1.5')
@@ -28,19 +31,45 @@ class Atm():
     _WEALTH_PERCENT: Decimal = Decimal('10')
 
     def __init__(self):
+        """Конструктор.        
+        """
+        self._count_operations: int = self._START_COUNT
         self._cash: Decimal = Decimal(self._ZERRO_RUR)
+
+    def _wealth_tax(self):
+        """Налогообложение.        
+        """
+        if self._cash > self._WEALTH_RUR:
+            _tax = round(self._cash * self._WEALTH_PERCENT / self._FULL_PERCENT,
+                         self._HUNDREDTH_PART)
+            self._cash -= _tax
+            print(f' You paid the wealth tax - {_tax} RUR')
+            self._balance()
+
+    def _counter_of_operations(self):
+        """Счетчик операций.        
+        """
+        _ADD_COUNT: int = int(1)
+        self._count_operations += _ADD_COUNT
+
+        if self._count_operations >= self._REMUNERATION_COUNT:
+            self._count_operations = self._START_COUNT
+            _remuneration = round(self._cash * self._REMUNERATION_PERCENT / self._FULL_PERCENT, self._HUNDREDTH_PART)
+            self._cash += _remuneration
+
+            print(f' Congratulations, you have received a reward of {_remuneration} RUR')
+            self._balance()
 
     def _balance(self):
         """Вывод суммы остатка средств (баланс).        
         """
-        print(f'Your money: {self._cash} RUR\n')
+        print(f' Your money: {self._cash} RUR\n')
 
     def _user_input_decimal(self, message: str) -> Decimal:
         """Пользовательский ввод суммы операции.
         :param message: Сообщение пользователю.
         :return: Сумма операции.
         """
-
         while True:
 
             _string = input(f'{message}').replace(',', '.')
@@ -55,30 +84,36 @@ class Atm():
                 if _decimal > 0 and not _decimal % self._MULTIPLICITY_RUR:
                     return _decimal
                 else:
-                    print(f'Cancel operation.')
+                    print(f' Cancel operation.')
 
             else:
-                print('You entered the amount in the wrong format!')
+                print(' You entered the amount in the wrong format!')
 
     def replenish(self):
         """Операция пополнения.        
         """
+        self._wealth_tax()
+
         message_for_user = (f''' Enter the amount of ATM replenishment multiple {self._MULTIPLICITY_RUR} RUR -> ''')
 
         self._cash += self._user_input_decimal(message_for_user)
 
         self._balance()
+        self._counter_of_operations()
 
     def withdraw(self):
         """Операция снятия.        
         """
+        self._wealth_tax()
+
         message_for_user = (f''' Withdrawal commision of ATM replenishment - {self._WITHDRAWAL_COMMISSION_PERCENT}%.
                            \r Minimum commission - {self._MIN_WITHDRAWAL_COMMISSION_RUR} RUR. 
                            \r Maximum commission - {self._MAX_WITHDRAWAL_COMMISSION_RUR} RUR.
                            \r Enter the amount of withdrawal from the ATM multiple {self._MULTIPLICITY_RUR} RUR -> ''')
 
         _request = self._user_input_decimal(message_for_user)
-        _operation_commission = _request * self._WITHDRAWAL_COMMISSION_PERCENT / self._FULL_PERCENT
+        _operation_commission = round(_request * self._WITHDRAWAL_COMMISSION_PERCENT / self._FULL_PERCENT,
+                                      self._HUNDREDTH_PART)
 
         if _operation_commission < self._MIN_WITHDRAWAL_COMMISSION_RUR:
             _operation_commission = self._MIN_WITHDRAWAL_COMMISSION_RUR
@@ -90,11 +125,13 @@ class Atm():
 
         if _request_sum <= self._cash:
             self._cash -= _request_sum
+            print(f' Operation commission - {_operation_commission} RUR')
 
         else:
-            print(f'Cancel operation - Insufficient funds on the account')
+            print(f' Cancel operation - Insufficient funds on the account')
 
         self._balance()
+        self._counter_of_operations()
 
     def close(self):
         """Выйти.        
@@ -128,7 +165,7 @@ def show_menu() -> int:
 
                     print()
                     return _number
-            print('You entered an invalid value\n')
+            print(' You entered an invalid value\n')
 
     MIN_CHOICE = 0
     MAX_CHOICE = 2
