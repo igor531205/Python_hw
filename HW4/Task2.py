ea3a55af23a92be7fd880629a6226bd79089b15d
igor531205 <41549374+igor531205@users.noreply.github.com>
2023-09-05 19:35:18 +0300

    Task2 complited

--- HW4/Task2.py
+++ HW4/Task2.py
@@ -1,3 +1,17 @@
 # Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь,
 # где ключ — значение переданного аргумента, а значение — имя аргумента. Если ключ не хешируем,
 # используйте его строковое представление.
+
+
+def key_parameters_to_dictionary(**kwargs):
+    """Функция перевода ключевых параметров в словарь."""
+
+    new_dict = {}
+    for key, value in kwargs.items():
+
+        new_dict[hash(value) if hash(value) == value else str(value)] = key
+
+    return new_dict
+
+
+print(key_parameters_to_dictionary(course='python', homework=4, task=2))
