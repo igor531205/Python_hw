5f1d5b719ea9ca9c6d5ce1a4c7e3e70671e1c01a
igor531205 <41549374+igor531205@users.noreply.github.com>
2023-09-05 19:19:16 +0300

    Task1 complited

--- HW4/Task1.py
+++ HW4/Task1.py
@@ -1,1 +1,15 @@
 # Напишите функцию для транспонирования матрицы.
+
+
+def matrix_transposition(matrix):
+    return list(list(i) for i in zip(*matrix))
+
+
+MATRIX = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(f'Old {MATRIX = }')
+print(f'New MATRIX = {matrix_transposition(MATRIX)}')
