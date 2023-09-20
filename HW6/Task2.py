96af2d0fdcd7ea11c30db2572650d7f422dc9fb7
igor531205 <41549374+igor531205@users.noreply.github.com>
2023-09-20 11:08:52 +0300

    HW6 complited

--- HW6/Task2.py
+++ HW6/Task2.py
@@ -12,6 +12,3 @@ if __name__ == '__main__':
 
     arrangements = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8)]
     print(f'''Ферзи {'бьют' if is_win(arrangements) else 'не бьют'} друг друга''')
-
-    arrangements = [(1, 1), (5, 2), (8, 3), (6, 4), (3, 5), (7, 6), (2, 7), (4, 8)]
-    print(f'''Ферзи {'бьют' if is_win(arrangements) else 'не бьют'} друг друга''')
